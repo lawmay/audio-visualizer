691cc18920870c2543c7cf4c538f07d0843bbce7
LM <lmay01@gmail.com>
2014-07-07 12:06:37 -0700

    add an audio context

--- app/app.js
+++ app/app.js
@@ -1,3 +1,29 @@
+console.log('start app.js');
 
 
-console.log('im here');
+
+var contextClass = (window.AudioContext || 
+  window.webkitAudioContext || 
+  window.mozAudioContext || 
+  window.oAudioContext || 
+  window.msAudioContext)
+;
+
+if (contextClass) {
+  var context = new contextClass(); // Web Audio API is available.
+} else {
+  // *** * ** *** ** ** * **** ** * ** *
+  // Web Audio API is not available. Ask the user to use a supported browser.
+
+  // this needs testing...
+}
+
+
+console.log(context);
+
+
+
+
+
+
+console.log('end app.js');
