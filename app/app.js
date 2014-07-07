d701021340c08d75331466ab8cc7ccdf00bec26e
LM <lmay01@gmail.com>
2014-07-07 16:35:31 -0700

    refactored some of the d3 code

--- app/app.js
+++ app/app.js
@@ -32,8 +32,8 @@ var createContextNodes = function(audio, context) {
 
   // create gain, filter and analyser nodes
   var gain = context.createGainNode();
-  gain.gain.value = 0.05;
-  // gain.gain.value = 0.5;
+  // gain.gain.value = 0.01;
+  gain.gain.value = 0.5;
   console.log(gain);
 
   // CREATE FILTER???
@@ -71,21 +71,33 @@ var audio = new Audio();
 
 createContextNodes(audio, context);
 
-audio.src = 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3';
+var audioSrc = [
+    'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3',
+    'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3',
+    'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3',
+    'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3',
+    'http://freedownloads.last.fm/download/569330114/Lost%2BBoys.mp3',
+    'http://freedownloads.last.fm/download/384950466/Thirteen%2BThirtyfive.mp3'
+];
 
 
 
+audio.src = audioSrc[2];
 
-var totalBars = 30;
+
+var totalBars = 70;
 var frequencyData = new Uint8Array(totalBars);
-
 
 var h = 400;
 var w = 1000;
 var myData = _.range(0, totalBars);
 
-var barWidth = 85;    // as a percentage
-var barScale = (barWidth/100);
+
+var xScale = d3.scale.ordinal()
+  .domain(myData)     // is the number of data elements
+  .rangeBands([0, w], 0.1, 0)    // 2nd parameter is padding between bars
+;
+
 
 var yScale = d3
   .scale.linear()
@@ -99,6 +111,7 @@ window.addEventListener('load', onLoad, false);
 function onLoad() {
 
 
+  // initialize d3 stuff when page loads
   var svg = d3
     .select(".svg-container")
     .attr("class", "svg-container")
@@ -112,24 +125,18 @@ function onLoad() {
     .data(myData)
     .enter()
     .append("rect")
-    .attr('x',function(d, i) { return i * 30; } )
-    // .attr("y", 200 )
-    .attr('y',function(d, i) { return yScale(d); } )
-    .attr('width', 20 )
-    .attr("height", function(d) { return h - yScale(d); } )
   ;
 
+ 
   function renderFrame() {
    requestAnimationFrame(renderFrame);
    analyser.getByteFrequencyData(frequencyData);
 
     rect
       .data(frequencyData)
-      .attr('x',function(d, i) { return i * 30; } )
-      // .attr("y", 200 )
+      .attr('x',function(d, i) { return xScale(i); } )
       .attr('y',function(d, i) { return yScale(d); } )
-      .attr('width', 20 )
-      // .attr("height", function(d) { return d; } );
+      .attr('width', function(d, i) { return xScale.rangeBand(); } )
       .attr("height", function(d) { return h - yScale(d); } )
     ;
   }
