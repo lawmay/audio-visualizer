1e5e9f36b7d3101fa296db20ba93fbec93169139
LM <lmay01@gmail.com>
2014-07-07 12:28:29 -0700

    creating nodes and stuff

--- app/app.js
+++ app/app.js
@@ -2,24 +2,56 @@ console.log('start app.js');
 
 
 
-var contextClass = (window.AudioContext || 
-  window.webkitAudioContext || 
-  window.mozAudioContext || 
-  window.oAudioContext || 
-  window.msAudioContext)
-;
+
+
+
+// create new audio context
+var createContextClass = function() {
+  return (
+    window.AudioContext || 
+    window.webkitAudioContext || 
+    window.mozAudioContext || 
+    window.oAudioContext || 
+    window.msAudioContext
+    )
+  ;
+}
+
+var contextClass = createContextClass();
 
 if (contextClass) {
   var context = new contextClass(); // Web Audio API is available.
 } else {
   // *** * ** *** ** ** * **** ** * ** *
+  // this needs implementation...
+
   // Web Audio API is not available. Ask the user to use a supported browser.
 
-  // this needs testing...
+  
 }
 
 
 console.log(context);
+
+
+// create new HTML5 audio element
+var audio = new Audio();
+
+// create source node containing the audio element
+var source = context.createMediaElementSource(audio);
+
+// create gain, filter and analyser nodes
+var gain = context.createGainNode();
+var filter = context.createBiquadFilter();
+var analyser = context.createAnalyser();
+
+
+// gain.gain.value = 0.5;
+// console.log(gain.gain.value);
+
+
+
+
 
 
 
@@ -27,3 +59,7 @@ console.log(context);
 
 
 console.log('end app.js');
+
+
+
+
