67274f29e64b1a35bd97bb8fd028a3a32eff139e
LM <lmay01@gmail.com>
2014-07-07 23:50:36 -0700

    added circular visualization, tweaked CSS/HTML

--- app/app.js
+++ app/app.js
@@ -32,8 +32,8 @@ var createContextNodes = function(audio, context) {
                        // createMediaElementSource
   // create gain, filter and analyser nodes
   var gain = context.createGainNode();
-  gain.gain.value = 0.01;
-  // gain.gain.value = 0.5;
+  // gain.gain.value = 0.01;
+  gain.gain.value = 0.5;
   // console.log(gain);
 
   // CREATE FILTER???
@@ -92,7 +92,7 @@ var totalBars = 70;
 var frequencyData = new Uint8Array(totalBars);
 
 var h = 400;
-var w = 900;
+var w = 1000;
 var myData = _.range(0, totalBars);
 
 
@@ -108,12 +108,57 @@ var yScale = d3
   .range([h, 0])      // 0 is second parameter to invert y-axis
 ;
 
-var z = d3.scale.ordinal()
-  .domain(totalBars)
-  .range(colorbrewer.RdBu[9])
-;
+// var z = d3.scale.ordinal()
+//   .domain(totalBars)
+//   .range(colorbrewer.RdBu[9])
+// ;
+
+
+
 
 window.addEventListener('load', onLoad, false);
+
+// var currentColor = '#ffccaa';
+var currentColor = '#eee';
+
+
+
+function loadD3() {
+  // initialize d3 stuff when page loads
+  svg = d3
+    .select(".svg-container")
+    .attr("class", "svg-container")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+  ;
+
+  // var rect = svg
+  //   .selectAll("rect")
+  //   .data(myData)
+  //   .enter()
+  //   .append("rect")
+  //   .attr("class", "rect")
+  // ;
+
+  var circ = svg
+    .selectAll("circle")
+    .data(myData)
+    .enter()
+    .append("circle")
+    .attr("class", "circ")
+  ;
+
+  // return rect;
+  return circ;
+}
+
+var svg;
+var currentShape = 'circ';
+// var rect;
+// var circ;
+
+
 
 function onLoad() {
 
@@ -124,15 +169,31 @@ function onLoad() {
       loadAndPlay(track_url);
   };
 
+  
 
-  // initialize d3 stuff when page loads
-  var svg = d3
-    .select(".svg-container")
-    .attr("class", "svg-container")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
+  var currentElement = loadD3();
+var switchToCircs = function() {
+  d3.selectAll('rect').remove();
+  d3.selectAll('circle').remove();
+
+  var circ = svg
+    .selectAll("circle")
+    .data(myData)
+    .enter()
+    .append("circle")
+    .attr("class", "circ")
   ;
+
+  currentShape = 'circ';
+  currentElement = circ;
+  // renderFrame();
+  // requestAnimationFrame(renderFrame);
+  // return circ;
+}
+
+var switchToRects = function() {
+  d3.selectAll('circle').remove();
+  d3.selectAll('rect').remove();
 
   var rect = svg
     .selectAll("rect")
@@ -140,33 +201,71 @@ function onLoad() {
     .enter()
     .append("rect")
     .attr("class", "rect")
+    // .transition()
   ;
 
- 
+  currentShape = 'rect';
+  currentElement = rect;
+  // requestAnimationFrame(renderFrame);
+  // renderFrame();
+  // return rect;
+}
+
   function renderFrame() {
-   requestAnimationFrame(renderFrame);
+   // requestAnimationFrame(renderFrame);
    analyser.getByteFrequencyData(frequencyData);
 
-    rect
-      .data(frequencyData)
-      // .transition()
-      // .duration(timeThingy)
-      .attr('x',function(d, i) { return xScale(i); } )
-      .attr('y',function(d, i) { return yScale(d); } )
-      .attr('width', function(d, i) { return xScale.rangeBand(); } )
-      .attr('height', function(d) { return h - yScale(d); } )
-      .style('fill', function(d, i) { return z(i); } )
-    ;
-
+   if (currentShape === 'rect') {
+      currentElement
+        .data(frequencyData)
+        // .transition()
+        // .duration(1)
+        .attr('x',function(d, i) { return xScale(i); } )
+        .attr('y',function(d, i) { return yScale(d); } )
+        .attr('width', function(d, i) { return xScale.rangeBand(); } )
+        .attr('height', function(d) { return h - yScale(d); } )
+        // .style('fill', function(d, i) { return z(i); } )
+        .style('fill', function(d, i) { return currentColor; } )
+      ;    
+   } else if (currentShape === 'circ') {
+      currentElement
+        .data(frequencyData)
+        // .transition()
+        // .duration(40)
+        .attr('cx',function(d, i) { var tempXScale = xScale(i); return tempXScale; } )
+        .attr('cy',function(d, i) { return yScale(i * (w / (totalBars*4))); } )
+        .attr('r',function(d, i) { return d / 1.8; } )
+        .style('fill', function(d, i) { return currentColor; } )
+      ;        
+   }
   }
+
+
+
+
 
   // var self = this;
   audio.play();
+  setInterval(renderFrame, 20);
   // setInterval(renderFrame.bind(self,1), 1);
-  renderFrame();
+  // renderFrame();
+
+
+
+var circleButton = document.getElementById('circleButton');
+circleButton.addEventListener('click', switchToCircs, false);
+// function switchCircs(e) {
+//   switchToCircs();
+// };
+
+var rectButton = document.getElementById('rectButton');
+rectButton.addEventListener('click', switchToRects, false);
+// function switchCircs(e) {
+//   switchToRects();
+// };
 
     window.addEventListener("keydown", keyControls, false);
-     
+
     function keyControls(e) {
 
         switch(e.keyCode) {
@@ -180,14 +279,17 @@ function onLoad() {
                     audio.pause();
                 }
                 break;
-            // case 37:
-            //     // left key pressed
-            //     loader.directStream('backward');
-            //     break;
-            // case 39:
-            //     // right key pressed
-            //     loader.directStream('forward');
-            //     break;
+            case 37:
+                // left key pressed
+                console.log('left key');
+                switchToRects();
+                break;
+            case 39:
+                // right key pressed
+                // loader.directStream('forward');
+                console.log('right key');
+                switchToCircs();
+                break;
         }   
     }
 
@@ -217,11 +319,13 @@ function loadAndPlay(track_url) {
           console.log('inside tracks get');
           // var soundCloudSrc = sound.stream_url + '?client_id=' + client_id;
           var soundCloudSrc = track.stream_url + '?client_id=' + client_id;
-          console.log(soundCloudSrc);
+          // console.log(soundCloudSrc);
           audio.setAttribute('src', soundCloudSrc);
           // audio.src = soundCloudSrc;
-          console.log(audio);
-
+          // console.log(audio);
+          document.getElementById('song-title').innerHTML = track.title;
+          // console.log();
+          // document.getElementById('song-title').value = 'bblas';
           audio.play();
           // renderFrame();
           // source.mediaElement.play();
