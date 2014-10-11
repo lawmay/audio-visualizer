21497462be982ce7050258edcc02ae65df328195
LM <lmay01@gmail.com>
2014-10-11 09:26:50 -0700

    add jshint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon')
+    nodemon = require('gulp-nodemon'),
+    jshint = require('gulp-jshint')
 ;
 
-gulp.task('default', function() {
+gulp.task('serve', function() {
   nodemon({script: './app/server.js', ignore: 'node_modules/**/*.js'})
     .on('restart', function () {
 
     });
 });
+
+
+// lint out JavaScript
+gulp.task('lint', function () {
+  return gulp.src('./app/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+});
+
+
+gulp.task('default', ['lint', 'serve']);
